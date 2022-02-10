4c126f9360d62e8b2cfc48d0817eb79e9b0051e1
insideoutzombie <daviddbruno9@gmail.com>
2022-02-10 14:34:58 -0500

    minor change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Success!!
+          Bitcoin Historic Chart
         </p>
         <a
           className="App-link"
